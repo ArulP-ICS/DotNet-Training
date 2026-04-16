b92832dc9a90ad65d622375c42f3443b6efc3e93
Arul Palanisamy <arulp@infinite.com>
2026-04-16 11:06:05 +0530

    \Assessment3 Added

---
+++ DotNet/Assessments/Assessment3/Program1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assessment3
+{
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Enter number of matches played: ");
+            int matches = Convert.ToInt32(Console.ReadLine());
+
+            CricketTeam team = new CricketTeam();
+            team.Pointscalculation(matches);
+
+            Console.ReadLine();
+        }
+    }
+
+    class CricketTeam
+    {
+        public void Pointscalculation(int no_of_matches)
+        {
+
+            int sum = 0;
+            int score;
+
+
+            for (int i = 1; i <= no_of_matches; i++)
+            {
+                Console.Write("Enter score for match " + i + ": ");
+                score = int.Parse(Console.ReadLine());
+                sum += score;
+            }
+
+
+            double average = (double)sum / no_of_matches;
+
+            Console.WriteLine("\n--- IPL Team Statistics ---");
+            Console.WriteLine("Total Matches Played: " + no_of_matches);
+            Console.WriteLine("Sum of Points: " + sum);
+            Console.WriteLine("Average Points: " + average);
+
+        }
+    }
+
+
+}
---
+++ DotNet/Assessments/Assessment3/Program2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace Assessment3
+{
+    class Program2
+    {
+        static void Main()
+        {
+            string filePath =
+@"C:\Users\arulp\ICS Training 2026\DotNet\Assessments\Assessment3\Outputs\Program 2 output\Assessment3File.txt";
+
+            Console.Write("Enter text to append to the file: ");
+            string text = Console.ReadLine();
+
+            // Append mode = true (creates file if not exists)
+            using (StreamWriter writer = new StreamWriter(filePath, true))
+            {
+                writer.WriteLine(text);
+            }
+
+            Console.WriteLine("Text successfully appended to the file.");
+            Console.ReadLine();
+        }
+    }
+}
